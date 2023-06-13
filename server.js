7000cc79aa1dbd5f119a6927308ee40f7a1e5c1c
RADWA ABOUDINA <radwa.aboudina@gmail.com>
2023-06-13 01:11:02 -0700

    run

--- server.js
+++ server.js
@@ -7,11 +7,10 @@ app.use(express.static(__dirname + '/'));
 app.get('/',function(req,res) {
     res.sendFile(__dirname +'/index.html');
   });
-//app.use(express.static("index.html"));
 
-// app.listen(PORT, function(err){
-//     if (err) console.log(err);
-//     console.log("Server listening on PORT", PORT);
-// });
+app.listen(PORT, function(err){
+    if (err) console.log(err);
+    console.log("Server listening on PORT", PORT);
+});
 
 
