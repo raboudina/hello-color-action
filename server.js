83160c3136fc9a4b5d3d4325a8cc510cb8273a25
RADWA ABOUDINA <radwa.aboudina@gmail.com>
2023-06-12 23:23:37 -0700

    changes testing file

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.get('/',function(req,res) {
   });
 //app.use(express.static("index.html"));
 
-app.listen(PORT, function(err){
-    if (err) console.log(err);
-    console.log("Server listening on PORT", PORT);
-});
+// app.listen(PORT, function(err){
+//     if (err) console.log(err);
+//     console.log("Server listening on PORT", PORT);
+// });
 
 
---
+++ server.test.js
@@ -0,0 +1,28 @@
+// Import Express and Supertest
+const express = require('express');
+const request = require('supertest');
+
+// Create an Express app that displays hello world
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// Write a test case using Jest and Supertest
+test('responds with hello world on the screen', async () => {
+  // Send a GET request to the app
+  const response = await request(app).get('/');
+  // Expect the status code to be 200
+  expect(response.statusCode).toBe(200);
+  // Expect the response text to contain hello world
+  expect(response.text).toContain('Hello World!');
+});
+
+var server = app.listen(5000, function(){
+        console.log('Running on port 5000!')
+     });
+
+afterAll(done=>{
+    server.close();
+    done();
+});
